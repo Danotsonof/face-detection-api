6448e9b9e462064ee157cb944389721f26f55481
Daniel Otulagun <Danotsonof@gmail.com>
2020-05-04 19:39:07 +0100

    changing password

--- server.js
+++ server.js
@@ -2,24 +2,27 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const knex = require("knex");
-/* for development mode
+
+// use for development mode
 const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1",
-    user: "postgres",
-    password: "dands1@P",
-    database: "face-detection"
+    user: "postgres", // change as approppriate
+    password: "password", // change password
+    database: "face-detection" // change name 
   }
-});*/
+});
 
+/* for deployment on heroku
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
   }
-});
+});*/
+
 
 const register = require('./controllers/register')
 const deleteAccount = require('./controllers/delete')
@@ -32,7 +35,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 3000;
+const port = 3001;
 
 // app.get("/", (req, res) => res.send('success'));
 app.post("/register", (req, res) => {register.handleRegister(req, res, db, bcrypt)});
